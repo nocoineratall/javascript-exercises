d382368e965c2a4c4b3f48173090469bb693ac40
nocoineratall <andrears90@hotmail.it>
2024-02-28 22:51:28 +0100

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (stringToBeRepeated, times) {
+  let output = "";
+  if (times >= 0) {
+    for (let i = 0; i < times; i++) {
+      output += stringToBeRepeated;
+    }
+    return output;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
