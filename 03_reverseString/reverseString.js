f0ffd89a7453519bf220a298977369acecd0f363
nocoineratall <andrears90@hotmail.it>
2024-02-28 22:51:54 +0100

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (toBeReversed) {
+  let stringLength = toBeReversed.length;
+  let reversed = "";
 
+  for (let i = stringLength - 1; i >= 0; i--) {
+    reversed += toBeReversed.at(i);
+  }
+
+  return reversed;
 };
 
 // Do not edit below this line
