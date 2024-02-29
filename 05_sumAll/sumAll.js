2d229f86628036cecf7f174eb972e605a5ee5ac8
nocoineratall <andrears90@hotmail.it>
2024-02-29 19:05:48 +0100

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  let arrayOfAll = [];
+  let sum = 0;
 
+  if (start < 0 || end < 0) {
+    return "ERROR";
+  } else if (typeof start != "number" || typeof end != "number") {
+    return "ERROR";
+  } else {
+    if (start > end) {
+      // reverses start with end so that end > start
+      let hold = end;
+      end = start;
+      start = hold;
+    }
+
+    //builds an array made of integers from start to end
+    for (let i = start; i <= end; i++) {
+      arrayOfAll.push(i);
+    }
+
+    for (let j = 0; j < arrayOfAll.length; j++) {
+      sum += arrayOfAll[j];
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
