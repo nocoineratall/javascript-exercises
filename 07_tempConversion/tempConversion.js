3979d407d7e44d79f106f5fd5f5758b884b605ae
nocoineratall <andrears90@hotmail.it>
2024-02-29 20:18:15 +0100

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  let rawTemp = ((temperature - 32) * 5) / 9;
+  let decimals = rawTemp - Math.floor(rawTemp);
+  let decimalsByTenRounded = Math.round(decimals * 10);
+  let roundTemp = Math.floor(rawTemp) + decimalsByTenRounded / 10;
+  return roundTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  let rawTemp = (temperature * 9) / 5 + 32;
+  let decimals = rawTemp - Math.floor(rawTemp);
+  let decimalsByTenRounded = Math.round(decimals * 10);
+  let roundTemp = Math.floor(rawTemp) + decimalsByTenRounded / 10;
+  return roundTemp;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
